fee94b48bae384fbbf106f1dd450da4d32bc05ca
SUPerchokki <32721115+SUPerchokki@users.noreply.github.com>
2022-04-20 16:05:34 -0300

    praticando a funcao random

---
+++ potenciometro e random.ino
@@ -0,0 +1,107 @@
+// C++ code
+//
+
+
+//leds e botoes
+#define LED_VERMELHO 2
+#define BOTAO_VERMELHO 3
+#define LED_VERDE 4
+#define BOTAO_VERDE 5
+#define LED_AMARELO 6
+#define BOTAO_AMARELO 7
+#define LED_AZUL 8
+#define BOTAO_AZUL 9
+
+#define INDEFINIDO -1
+
+//tempo
+#define UM_SEGUNDO 1000
+#define MEIO_SEGUNDO 500
+
+//sequencia
+int sequencia[4];
+
+int sorteiaCor(){
+  int i = random(LED_VERMELHO, BOTAO_AZUL);
+  while(i % 2 == 1){
+    i = random(LED_VERMELHO, BOTAO_AZUL);
+  }
+  return(i);
+}
+
+
+void iniciaJogo(){
+  int jogo = analogRead(0);
+  randomSeed(jogo);
+  for(int i = 0; i < 4; i++){
+    sequencia[i] = sorteiaCor();
+  }
+
+}
+
+void iniciaPortas(){
+  for(int i = 2; i < 10; i ++){
+    if(i % 2 == 0){
+    	pinMode(i, OUTPUT);
+    }else if(i % 2 == 1){
+    	pinMode(i++, INPUT_PULLUP);
+    } 
+  } 
+}
+
+void testeBotao(){
+    for(int i = 2; i < 9; i += 2){
+    digitalWrite(i, HIGH);
+    delay(500);
+    digitalWrite(i, LOW);
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  iniciaPortas();
+  iniciaJogo();
+}
+
+int piscaLed(int porta){
+	digitalWrite(porta, HIGH);
+	delay(500);
+  	digitalWrite(porta, LOW);
+  	delay(500);
+  	return(porta);
+}
+
+int estadoBotao(){
+  int estadoVermelho = digitalRead(BOTAO_VERMELHO);
+  if(estadoVermelho == LOW){
+    return(piscaLed(LED_VERMELHO));
+  }
+  
+  int estadoVerde = digitalRead(BOTAO_VERDE);
+  if(estadoVerde == LOW){
+    return(piscaLed(LED_VERDE));
+  }
+  
+  int estadoAmarelo = digitalRead(BOTAO_AMARELO);
+  if(estadoAmarelo == LOW){
+  	return(piscaLed(LED_AMARELO));
+  }
+  
+  int estadoAzul = digitalRead(BOTAO_AZUL);
+  if(estadoAzul == LOW){
+  	return(piscaLed(LED_AZUL));
+  } else {
+    return(INDEFINIDO);
+  }
+}
+
+
+void loop()
+{
+  for(int i = 0;i < 4; i++){
+    piscaLed(sequencia[i]);
+  }
+ 
+}
